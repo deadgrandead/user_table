4e312c6997aef4810dfb8b0be23ca892c5282893
Andrew Romanov <92754319+deadgrandead@users.noreply.github.com>
2023-10-20 20:15:15 +0300

    Add files via upload

---
+++ create_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE PERSONS (
+    name VARCHAR(50),
+    surname VARCHAR(50),
+    age INT,
+    phone_number VARCHAR(20),
+    city_of_living VARCHAR(50),
+    PRIMARY KEY (name, surname, age)
+);
